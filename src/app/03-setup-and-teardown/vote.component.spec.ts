139692b736c385f2e8e21a9ea4ffea63920e2010
Menik <menikdisney@gmail.com>
2019-04-25 13:21:59 +0600

    Testing component(Setup and teardown)

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,66 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
+
+/*
+describe('VoteComponent', () => {
+  it('should increment totalVotes when upvoted', () => {
+
+    // Assert
+    const component = new VoteComponent();
+
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+
+    // Assert
+    const component = new VoteComponent();
+
+    // Act
+    component.downVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(-1);
+
+  });
+
+});
+
+*/
+
+/*Better way*/
+
+let component: VoteComponent;
+
+beforeEach(() => {
+
+  // Arrange
+  component = new VoteComponent();
+});
 
 describe('VoteComponent', () => {
-  it('', () => {
+  it('should increment totalVotes when upvoted', () => {
+
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+
   });
 
-  it('', () => {
+  it('should decrement totalVotes when downvoted', () => {
+
+    // Act
+    component.downVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(-1);
+
   });
+
 });
