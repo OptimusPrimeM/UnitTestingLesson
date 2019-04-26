c9a77b8fd73adf0095dc7367a7ff698c270c9981
Menik <menikdisney@gmail.com>
2019-04-26 13:07:59 +0600

    Using spyOn for mocking service

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,33 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { from } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with the item returned from the serve', () => {
+
+    // Arrage
+    const todos = [1, 2, 3, 4];
+    spyOn(service, 'getTodos')
+      .and
+      .callFake(() => {
+        return from([todos]);
+      });
+
+    // Action
+    component.ngOnInit();
+
+    // Assertion
+    expect(component.todos.length).toBeGreaterThan(0);
+    expect(component.todos.length).toBe(4);
+    expect(component.todos).toBe(todos);
+
   });
 });
